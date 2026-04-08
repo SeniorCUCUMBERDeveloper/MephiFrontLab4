88e39f1d76bd9fa0e5d300072301cfe7fccd5cd0
Senior <kalinenkov.a@bk.ru>
2026-04-08 16:49:35 +0300

    first commit

---
+++ 4/2/scripts/script.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('number-container');
+let html = '';
+for (let i = 1; i <= 100; i++) {
+  html += `<span class="number-item">${i}</span> `;
+}
+container.innerHTML = html;
